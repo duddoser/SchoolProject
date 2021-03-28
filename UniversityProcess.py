a03ac0e28413cab743bc43aa9890de2bdef17597
duddoser <45271863+duddoser@users.noreply.github.com>
2021-03-28 19:39:12 +0200

    Update UniversityProcess.py

--- UniversityProcess.py
+++ UniversityProcess.py
@@ -9,22 +9,6 @@ SUBJECTS = {'Математика': 'egemat', 'Русский язык': 'egerus
             'Информатика': 'egeinform', 'Биология': 'egebiol', 'Химия': 'egehim', 'Обществознание': 'egeobsh',
             'История': 'egeist', 'Иностранный язык': 'egeinyaz', 'Литература': 'egeliter', 'География': 'egegeorg'}
 
-
-class University:
-
-    def get_universities(self):
-        page = requests.get(URL + RATE)
-        soup = BeautifulSoup(page.content, 'html.parser')import requests
-from bs4 import BeautifulSoup
-
-URL = 'https://vuzopedia.ru'
-RATE = '/rate'
-EGE = '/kakie-ege-sdavat'
-POEGE = '/poege/'
-SUBJECTS = {'Математика': 'egemat', 'Русский язык': 'egerus', 'Физика': 'egefiz', 'Вступительные': 'vstup',
-            'Информатика': 'egeinform', 'Биология': 'egebiol', 'Химия': 'egehim', 'Обществознание': 'egeobsh',
-            'История': 'egeist', 'Иностранный язык': 'egeinyaz', 'Литература': 'egeliter', 'География': 'egegeorg'}
-
 UNIVERSITY_PICS = {'Московский государственный университет имени М.В. Ломоносова': 'https://vuzopedia.ru/storage/app/uploads/public/6fe/fe8/729/thumb__59_59_0_0_auto.png',
                    'Московский физико-технический институт (Государственный университет)': 'https://vuzopedia.ru/storage/app/uploads/public/b48/2e2/3a7/thumb__59_59_0_0_auto.png',
                    'Национальный исследовательский ядерный университет «МИФИ»': 'https://vuzopedia.ru/storage/app/uploads/public/258/27d/4e3/thumb__59_59_0_0_auto.png',
@@ -104,49 +88,4 @@ class University:
                             pass
                 if result != {}:
                     response[university] = [result, UNIVERSITY_PICS[university]]
-        return response
-
-        uni_rate = soup.find_all('div', {'class': 'col-md-12 vuzRateItem'})
-
-        universities = {}
-        k = 1
-        for title in uni_rate:
-            for a in title.find_all('a', href=True):
-                uni = title.find('a').text.strip('>')
-                if a['href'] != '/vuz/1415':
-                    universities[uni] = {k: a['href']}
-            if k == 30:
-                return universities
-            k += 1
-
-    def get_faculties(self, universities, request):
-        response = {}
-
-        for university in universities:
-            for ref in universities[university]:
-                req = ''
-                for el in request:
-                    req += SUBJECTS[el] + ';'
-
-                page = requests.get(URL + universities[university][ref] + POEGE + req)
-                soup = BeautifulSoup(page.content, 'html.parser')
-                faculties = soup.find_all('div', {'class': 'itemSpecAll'})
-
-                result = {}
-                for fac in faculties:
-                    for title in fac.find_all('a', {'class': 'spectittle'}):
-                        try:
-                            additional = fac.find_all('a', {'class': 'tooltipq'})[2].text.strip('<')
-                            additional2 = fac.find_all('a', {'class': 'tooltipq'})[1].text.strip('<')
-                            if additional[3] != '-':
-                                try:
-                                    result[title.text.strip('<')] = int(additional[3:6])
-                                except ValueError:
-                                    if additional2[3:6][0] in '1234567890' and additional2[3:6][1] in '1234567890' and \
-                                            additional2[3:6][2] in '1234567890':
-                                        result[title.text.strip('<')] = int(additional2[3:6])
-                        except IndexError:
-                            pass
-                if result != {}:
-                    response[university] = result
         return response
