b3e751d068db55de47035426e4b278954bec5d69
duddoser <45271863+duddoser@users.noreply.github.com>
2021-03-31 10:53:15 +0200

    added get_universities

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from UniversityProcess import University
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'android_secret_key'
+app.config['SECRET_KEY'] = 'android_game_secret_key'
 uni_process = University()
-UNIVERSITY_DICT = uni_process.get_universities()
 
 
 @app.errorhandler(404)
@@ -13,13 +12,18 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-@app.route('/get_universities', methods=["POST"])
+@app.route('/get_unversities', methods=["POST"])
 def get_universities():
+    university_list = uni_process.get_universities()
+    return jsonify({'universities': university_list})
+
+
+@app.route('/get_faculties', methods=["POST"])
+def get_faculties():
     result = request.get_json()
-    subjects = []
-    for el in result:
-        subjects.append(el)
-    snippet = uni_process.get_faculties(UNIVERSITY_DICT, subjects)
+    subjects = [s for s in result['subjects']]
+    unis = [u for u in result['universities']]
+    snippet = uni_process.get_faculties(unis, subjects)
     return jsonify(snippet)
 
 
