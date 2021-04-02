bda18f34017796759993edbaf62538f4a2156ec2
duddoser <45271863+duddoser@users.noreply.github.com>
2021-04-02 21:39:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
-from flask import Flask, make_response, jsonify, request
+import json
+from collections import defaultdict
+
+
 from UniversityProcess import University
 
+university_mod = University()
+universities_list = university_mod.get_universities()
+subjects = ['Физика', 'Информатика', 'Биология', 'Химия', 'Обществознание', 'История',
+            'Иностранный язык', 'Литература', 'География', 'Вступительные']
+# прогнать по одному вузу каждую комбинацию
+result = defaultdict(list)
+u = 'Московский государственный университет имени М.В. Ломоносова'
+rate = 1
+for i in range(len(subjects)):
+    subjects_now = ['Математика', 'Русский язык', subjects[i]]
+    response = university_mod.get_faculties(universities_list, subjects_now)
+    print(response)
+    for j in range(i + 1, len(subjects)):
+        subjects_now = ['Математика', 'Русский язык', subjects[i], subjects[j]]
+        response = university_mod.get_faculties(universities_list, subjects_now)
+        print(response)
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'android_game_secret_key'
-uni_process = University()
-
-
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
-
-
-@app.route('/get_universities', methods=["POST"])
-def get_universities():
-    university_list = uni_process.get_universities()
-    return jsonify({'universities': university_list})
-
-
-@app.route('/get_faculties', methods=["POST"])
-def get_faculties():
-    result = request.get_json()
-    subjects = [s for s in result['subjects']]
-    unis = [u for u in result['universities']]
-    snippet = uni_process.get_faculties(unis, subjects)
-    return jsonify(snippet)
+        for k in range(j + 1, len(subjects)):
+            subjects_now = ['Математика', 'Русский язык', subjects[i], subjects[j], subjects[k]]
+            response = university_mod.get_faculties(universities_list, subjects_now)
 
 
-if __name__ == '__main__':
-    app.run()
+
+# with open('data.json', 'w', encoding='utf-8') as f:
+#     json.dump(result, f, ensure_ascii=False, indent=4)
+
